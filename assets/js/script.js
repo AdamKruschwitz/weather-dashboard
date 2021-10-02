7cb969ab04ef70d3f94c1c9fdd9be30a2a03c298
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-02 13:35:42 -0700

    added pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Pseudocode
+
+// Get variables for the forecast card container, the search input and button, and current forecast, and a default city
+
+// Make an API call to get the weather for the default city
+// parse that content and set for the current conditions and the forecast elements on the page
+// When the user clicks the submit button...
+    // Get the value of the input field
+    // Save the value of the input field to the history in localStorage and update the history on the page
+    // Make an API call to get the weather for the city in the input field
