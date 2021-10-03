5ac36f06960a97124cbd60f76a924562c9f916e9
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-02 17:33:37 -0700

    programmed api calls for geocoder and oneCall weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,63 @@
 // Pseudocode
 
 // Get variables for the forecast card container, the search input and button, and current forecast, and a default city
+var searchBtn = $("button");
+var forecastContainer = $("#forecast-container");
+
+var openWeatherApiKey = "b11f5ffe66d7025a0419eaeb90671962";
 
 // Make an API call to get the weather for the default city
+let currentWeather = runSearch("San Francisco");
+
+console.log(currentWeather);
 // parse that content and set for the current conditions and the forecast elements on the page
 // When the user clicks the submit button...
     // Get the value of the input field
     // Save the value of the input field to the history in localStorage and update the history on the page
     // Make an API call to get the weather for the city in the input field
+
+
+
+/**
+ * @description Returns the data from a OneCall API query from the given city.
+ * @param {String} city 
+ */
+function getOneCallData(lat, lon) {
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + openWeatherApiKey;
+
+    fetch(url).then(response => response.json()).then(function(data) {
+        populateCurrentWeatherData(data.current.temp, data.current.humidity, data.current.uvi);
+
+    });
+}
+
+/**
+ * @description Returns the lattitude and longitude of the given city as an object.
+ * @param {String} city 
+ */
+function getCoordinates(city) {
+    let lat, lon;
+    url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + openWeatherApiKey;
+    fetch(url).then(response => response.json()).then(function(data) {
+        lat = data[0].lat;
+        lon = data[0].lon;
+        populateCurrentWeatherTitle(data[0].name + ", " + data[0].country);
+        getOneCallData(lat, lon);
+    });
+    return {"lat": lat, "lon":lon};
+}
+
+// TODO
+function populateCurrentWeatherData(temp, humidity, uvi) {
+    console.log("temp: " + temp + ", humidity: " + humidity + ", uvi: " +uvi);
+}
+
+// TODO
+function populateCurrentWeatherTitle(city) {
+    console.log(city);
+}
+
+// Kick off function for the page search
+function runSearch(city) {
+    getCoordinates(city);
+}
